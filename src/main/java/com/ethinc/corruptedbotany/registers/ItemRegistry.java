94f862cb4259d80a7b0a30c5a06b3455404ff0b8
Robert <recallggs@gmail.com>
2022-08-19 02:54:54 -0500

    0.0.0.08

--- src/main/java/com/ethinc/corruptedbotany/registers/ItemRegistry.java
+++ src/main/java/com/ethinc/corruptedbotany/registers/ItemRegistry.java
@@ -1,15 +1,20 @@
 package com.ethinc.corruptedbotany.registers;
 
 import com.ethinc.corruptedbotany.CorruptedBotany;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry {
 
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CorruptedBotany.MOD_ID);
+
+    public static final RegistryObject<Item> MINESSENCE = ITEMS.register("minuscule_essence",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
     public static void register(IEventBus eventBus) {
